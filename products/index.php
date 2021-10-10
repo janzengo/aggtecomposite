117aa3722f738dd6cf0ca48b1facc1a9dbea557c
Janzen <jjanzen.go@gmail.com>
2021-10-10 17:17:32 +0800

    Products Page Fixed Loading

--- products/index.php
+++ products/index.php
@@ -3,8 +3,14 @@
 <?php include 'includes/session.php'; ?>
 <div id="preload" class="preloader">
 </div>
+<style>
+	#invis {
+		display: none;
+	}
+</style>
+<div id="invis">
 <?php include 'includes/header.php'; ?>
-<body class="hold-transition layout-top-nav">
+<body class="hold-transition layout-top-nav" onload="load()">
 	<?php include 'category.php'; ?>
 	      <!-- Main content -->
 	        		<?php
@@ -17,15 +23,20 @@
 	        				unset($_SESSION['error']);
 	        			}
 	        		?>
-</div>
+
   	<?php include '../footer.php'; ?>
 <?php include 'includes/scripts.php'; ?>
 <?php include 'includes/links.php' ?>
+</div>
 <script>
     $(document).ready(function() {
         $('#preload').load(function() {
         });
     });
+	$(window).load(function(){
+    $('#invis').show();
+    $('.preloader').fadeOut(500);
+});
 </script>
 </body>
 </html>

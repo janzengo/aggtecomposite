e7bd4fa6766f6329cc0a9abc660c57542767809a
Janzen <jjanzen.go@gmail.com>
2021-11-09 20:27:50 +0800

    Image Addition to Portfolio

--- portfolio.php
+++ portfolio.php
@@ -471,6 +471,28 @@
           </div>
         </li>
         <!--col end-->
+        <!--col start-->
+        <li class="col-md-3 col-sm-6 col-xs-12 nf-item cladding">
+          <div class="image-hover-effect-4"> <img src="images/gallery/cladding/cladding_p.jpg.png" alt="img-8">
+            <div class="caption">
+              <h3>WPC Cladding</h3>
+              <p>Philippines</p>
+            </div>
+            <div class="link-wrap"> <a href="images/gallery/cladding/cladding_p.jpg" title="Accent Cladding" class="lightbox-image"><i class="fa fa-search"></i></a>  </div>
+          </div>
+        </li>
+        <!--col end-->
+        <!--col start-->
+        <li class="col-md-3 col-sm-6 col-xs-12 nf-item cladding">
+          <div class="image-hover-effect-4"> <img src="images/gallery/cladding/cladding_q.jpg.png" alt="img-8">
+            <div class="caption">
+              <h3>WPC Cladding</h3>
+              <p>Philippines</p>
+            </div>
+            <div class="link-wrap"> <a href="images/gallery/cladding/cladding_q.jpg" title="Accent Cladding" class="lightbox-image"><i class="fa fa-search"></i></a>  </div>
+          </div>
+        </li>
+        <!--col end-->
       </ul>
       <!--row end-->
     </div>

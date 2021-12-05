74cebb3d38495c6f5962208f002358bc23fa9d7d
Janzen <jjanzen.go@gmail.com>
2021-12-05 18:49:40 +0800

    Button

--- products/category.php
+++ products/category.php
@@ -1,3 +1,59 @@
+<style>
+	.label-container{
+	position:fixed;
+	bottom:48px;
+	right:105px;
+	display:table;
+	visibility: hidden;
+}
+.faFloat {
+	font-size: 50px;
+}
+.label-text{
+	color:#FFF;
+	background:rgba(51,51,51,0.5);
+	display:table-cell;
+	vertical-align:middle;
+	padding:10px;
+	border-radius:3px;
+}
+
+.label-arrow{
+	display:table-cell;
+	vertical-align:middle;
+	color:#333;
+	opacity:0.5;
+}
+
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#228b22;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+}
+
+.my-float{
+	font-size:40px;
+	margin-top:18px;
+}
+
+a.float + div.label-container {
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s, opacity 0.5s ease;
+}
+
+a.float:hover + div.label-container{
+  visibility: visible;
+  opacity: 1;
+}
+	</style>
 <?php require_once 'cart_modals.php'; ?>
 <?php
 	$conn = $pdo->open();
@@ -20,19 +76,19 @@
 	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 		                <ol class="carousel-indicators">
 		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+		                  <!--<li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>-->
 		                </ol>
 		                <div class="carousel-inner">
 		                  <div class="item active">
 		                    <img src="images/banner1.png" alt="First slide">
 		                  </div>
-		                  <div class="item">
+		                  <!--<div class="item">
 		                    <img src="images/banner2.png" alt="Second slide">
 		                  </div>
 		                  <div class="item">
 		                    <img src="images/banner3.png" alt="Third slide">
-		                  </div>
+		                  </div>-->
 		                </div>
 		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
 		                  <span class="fa fa-angle-left"></span>
@@ -132,10 +188,18 @@
 							if(count($_SESSION['cart']) != 0) {
 							echo"
 								<a class='buttonProdView' data-dismiss='modal' aria-label='Close' data-toggle='modal' data-target='.prodModal'><i class='fa fa-list'></i> View Products for Quotation</a>
+								<a class='float' data-dismiss='modal' aria-label='Close' data-toggle='modal' data-target='.prodModal'>
+						<i class='fa fa-list my-float' style='font-size: 30px;'></i>
+						</a>
+						<div class='label-container'>
+						<div class='label-text'>View Products for Quotation</div>
+						<i class='fa fa-play label-arrow'></i>
+					</div>
 	        					";
 							}
 						}
 	        		?>
+					
 	        </div>
 	        </div>
 	  </div>

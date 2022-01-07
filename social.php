aa6746c8760aeee7813325b39c4640d780f3a6ef
wikiwyrhead <arniel.go@gmail.com>
2022-01-07 15:12:57 +0800

    adjust social opacity

--- social.php
+++ social.php
@@ -11,10 +11,10 @@
 
       <div class="provized_social_float">
         <a href="https://www.facebook.com/sharer.php?u=https://aggtedeck.com" target="_blank" style="background: rgba(0,124,255,1.00)" class="provized_social_icon"><i class="fab fa-facebook-f"></i> </a>
-        <a href="https://twitter.com/intent/tweet?text=AGGTEDeck-Transforming Your Ideas Into Reality &url=https://aggtedeck.com &hashtags=wpc,woodplasticcomposite,wpcdecking,poolside,woodalternative,decking,cladding" target="_blank" style="background: rgba(0,187,255,1.00)" class="provized_social_icon"><i class="fab fa-twitter"></i> </a>
-        <a href="https://www.instagram.com/aggtedeck/" target="_blank" style="background: rgba(188,42,181,1.00)" class="provized_social_icon"><i class="fab fa-instagram"></i> </a>
-        <a href="http://pinterest.com/aggtedeck" target="_blank" style="background: rgba(255,0,0,1.00)" class="provized_social_icon"><i class="fab fa-pinterest"></i> </a>
-        <a href="https://www.linkedin.com/company/aggtedeck" target="_blank" style="background: rgba(0,119,181,1.00)" class="provized_social_icon"><i class="fab fa-linkedin-in"></i> </a>
+        <a href="https://twitter.com/intent/tweet?text=AGGTEDeck-Transforming Your Ideas Into Reality &url=https://aggtedeck.com &hashtags=wpc,woodplasticcomposite,wpcdecking,poolside,woodalternative,decking,cladding" target="_blank" style="background: rgba(0,187,255,1.1)" class="provized_social_icon"><i class="fab fa-twitter"></i> </a>
+        <a href="https://www.instagram.com/aggtedeck/" target="_blank" style="background: rgba(188,42,181,1.1)" class="provized_social_icon"><i class="fab fa-instagram"></i> </a>
+        <a href="http://pinterest.com/aggtedeck" target="_blank" style="background: rgba(255,0,0,1.1)" class="provized_social_icon"><i class="fab fa-pinterest"></i> </a>
+        <a href="https://www.linkedin.com/company/aggtedeck" target="_blank" style="background: rgba(0,119,181,1.1)" class="provized_social_icon"><i class="fab fa-linkedin-in"></i> </a>
       </div>
 	  
     </body>

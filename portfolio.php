cbfac04445f49a830a42c1f7b9950943d2827b11
Janzen <jjanzen.go@gmail.com>
2022-04-06 17:50:03 +0800

    New Images

--- portfolio.php
+++ portfolio.php
@@ -548,6 +548,17 @@
           </div>
         </li>
         <!--col end-->
+        <!--col start-->
+        <li class="col-md-3 col-sm-6 col-xs-12 nf-item decking">
+          <div class="image-hover-effect-4"> <img src="images/gallery/decking/newdecking4.jpg" alt="img-8">
+            <div class="caption">
+              <h3>WPC Decking</h3>
+              <p>Camp Aurora Lumban Laguna</p>
+            </div>
+            <div class="link-wrap"> <a href="images/gallery/decking/newdecking4.jpg" title="Accent Cladding" class="lightbox-image"><i class="fa fa-search"></i></a>  </div>
+          </div>
+        </li>
+        <!--col end-->
       </ul>
       <!--row end-->
     </div>
